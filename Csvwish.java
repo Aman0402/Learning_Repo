bf1434b715e8b85219e46200dcb2a67a3581c116
Aman Vishnoi <amanvishnoi1998@gmail.com>
2021-02-09 20:29:49 +0530

    Csv birthday wishes file is added

---
+++ Csvwish.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Csvwish {
+
+    public static void main(String[] args) {
+
+        readCsvUsingScanner("C:\\Users\\Aman Vishnoi\\Documents\\Book1.csv");
+    }
+
+    private static void readCsvUsingScanner(String filePath) {
+        try {
+            Scanner sc = new Scanner(new File(filePath));
+
+            if (sc.hasNext()) {
+                sc.next();
+            }
+
+            while (sc.hasNext()) {
+
+                System.out.println("Happy Birthday: " + sc.next().toString());
+            }
+            sc.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
