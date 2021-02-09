8ab27e839846c9be1b677e174518985fec8376f0
Aman0402 <68122752+Aman0402@users.noreply.github.com>
2021-02-09 20:57:31 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello
 {
     public static void main(String[] args) {
-        System.out.println("Hello aman finally we did it");
+        System.out.println("Hello aman now we changed in dev");
     }
 }
